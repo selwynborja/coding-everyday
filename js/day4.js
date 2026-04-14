a26e426ac6f2715455ade0428a9ae3051fd01e56
John Selwyn Borja <johnselwynjoe@gmail.com>
2026-04-14 08:36:34 +0800

    string lower and upper case using built in functions

---
+++ js/day4.js
@@ -0,0 +1,7 @@
+const str1 = 'helooooooo';
+const str2 = 'HELOOOOOOO';
+const str3 = 'nhoj whynles';
+
+console.log(str1.toUpperCase());
+console.log(str2.toLowerCase());
+console.log(str3.toUpperCase());
