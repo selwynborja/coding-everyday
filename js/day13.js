39f2cd363cbb7393ddd52842d461559d34f3b7d0
John Selwyn Borja <johnselwynjoe@gmail.com>
2026-04-24 20:23:39 +0800

    loop through numeric array printing both the index and value

---
+++ js/day13.js
@@ -0,0 +1,9 @@
+const numbers = [10, 20, 30, 40];
+
+// for(const [index, value] of numbers.entries()){
+//   console.log(`Index: ${index}, Value: ${value}`);
+// }
+
+for (let i = 0; i < numbers.length; i++){
+  console.log(`Index: ${i}, Value: ${numbers[i]}`);
+}
