55db68d5f773f4372244a127087027c5c33e3ade
John Selwyn Borja <selwynborja11@gmail.com>
2026-05-04 12:35:06 +0800

    (formerly day18) + count on a string

---
+++ js/day24.js
@@ -0,0 +1,13 @@
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+readline.question('Enter a string: ', (input) => {
+const uniqueChars = [...new Set(input)].join('');
+
+console.log(`Unique characters: ${uniqueChars}`);
+console.log(`Character Length: ${uniqueChars.length}`);
+
+readline.close();
+});
