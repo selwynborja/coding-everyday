86b785cad4cd48f2572de011cd22a44d50d36bef
John Selwyn Borja <johnselwynjoe@gmail.com>
2026-05-07 09:51:00 +0800

    Create functions for x and y: subtract, multiply, and divide

---
+++ python/day26.py
@@ -0,0 +1,12 @@
+def subtract(x, y):
+  return x - y
+
+def multiply(x, y):
+  return x * y  
+
+def divide(x, y):
+  return x / y
+
+print(subtract(12, 2))
+print(multiply(19, 2))
+print(divide(43, 5))
