93638f6b21d1b533c20275844ba164be9bb65644
John Selwyn Borja <johnselwynjoe@gmail.com>
2026-05-07 09:51:48 +0800

    Create functions for x and y: subtract, multiply, and divide

---
+++ js/day26.js
@@ -0,0 +1,15 @@
+function subtract(x, y){
+  return x - y;
+}
+
+function multiply(x, y){
+  return x * y;
+}
+
+function divide(x, y){
+  return x / y;
+}
+
+console.log(subtract(10, 5));
+console.log(multiply(10, 5));
+console.log(divide(10, 5));
