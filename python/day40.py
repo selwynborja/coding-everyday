3c910aef6cc2c84e5e7c5cd7ddbe29c65b7dff1f
John Selwyn Borja <selwynborja11@gmail.com>
2026-05-20 22:46:44 +0800

    Print out new york date and time

---
+++ python/day40.py
@@ -0,0 +1,5 @@
+from datetime import datetime
+from zoneinfo import ZoneInfo
+
+ny_time = datetime.now(ZoneInfo("America/New_York"))
+print("New York Date and Time: ", ny_time.strftime("%Y-%m-%d %H:%M:%S %Z"))
