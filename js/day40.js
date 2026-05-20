69533a2f2cadafec5a0fe7b9c49f1b70c80db96b
John Selwyn Borja <selwynborja11@gmail.com>
2026-05-20 22:47:18 +0800

    Print out new york date and time

---
+++ js/day40.js
@@ -0,0 +1,7 @@
+const nycTime = new Intl.DateTimeFormat('en-US', {
+  timeZone: 'America/New_York',
+  dateStyle: 'full',
+  timeStyle: 'long'
+}).format(new Date());
+
+console.log(`New York Date and Time: ${nycTime}`);
