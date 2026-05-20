1de403e7ca52d33ddb422f63832f78b4e5b7f35a
John Selwyn Borja <selwynborja11@gmail.com>
2026-05-20 23:16:37 +0800

    GGPAR

--- python/day40.py
+++ python/day40.py
@@ -3,8 +3,3 @@ from zoneinfo import ZoneInfo
 
 ny_time = datetime.now(ZoneInfo("America/New_York"))
 print("New York Date and Time: ", ny_time.strftime("%Y-%m-%d %H:%M:%S %Z"))
-
-print("nako po github")
-print("anlala")
-print("tangina")
-print("gg")
