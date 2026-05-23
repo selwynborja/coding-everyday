0fb002a058016d7fbc62d3f79a110fc7b24f3508
John Selwyn Borja <selwynborja11@gmail.com>
2026-05-23 15:37:27 +0800

    24 timezones list array of objects

---
+++ js/day43.js
@@ -0,0 +1,29 @@
+const timezones = [
+  { offset: -12, name: "Etc/GMT-12", label: "GMT-12:00" },
+  { offset: -11, name: "Pacific/Midway", label: "GMT-11:00" },
+  { offset: -10, name: "Pacific/Honolulu", label: "GMT-10:00" },
+  { offset: -9, name: "America/Anchorage", label: "GMT-09:00" },
+  { offset: -8, name: "America/Los_Angeles", label: "GMT-08:00" },
+  { offset: -7, name: "America/Denver", label: "GMT-07:00" },
+  { offset: -6, name: "America/Chicago", label: "GMT-06:00" },
+  { offset: -5, name: "America/New_York", label: "GMT-05:00" },
+  { offset: -4, name: "America/Halifax", label: "GMT-04:00" },
+  { offset: -3, name: "America/Sao_Paulo", label: "GMT-03:00" },
+  { offset: -2, name: "Atlantic/South_Georgia", label: "GMT-02:00" },
+  { offset: -1, name: "Atlantic/Azores", label: "GMT-01:00" },
+  { offset: 0, name: "Etc/UTC", label: "GMT+00:00" },
+  { offset: 1, name: "Europe/London", label: "GMT+01:00" },
+  { offset: 2, name: "Europe/Paris", label: "GMT+02:00" },
+  { offset: 3, name: "Europe/Moscow", label: "GMT+03:00" },
+  { offset: 4, name: "Asia/Dubai", label: "GMT+04:00" },
+  { offset: 5, name: "Asia/Karachi", label: "GMT+05:00" },
+  { offset: 6, name: "Asia/Dhaka", label: "GMT+06:00" },
+  { offset: 7, name: "Asia/Bangkok", label: "GMT+07:00" },
+  { offset: 8, name: "Asia/Singapore", label: "GMT+08:00" },
+  { offset: 9, name: "Asia/Tokyo", label: "GMT+09:00" },
+  { offset: 10, name: "Australia/Sydney", label: "GMT+10:00" },
+  { offset: 11, name: "Pacific/Noumea", label: "GMT+11:00" },
+  { offset: 12, name: "Pacific/Auckland", label: "GMT+12:00" }
+];
+
+console.log(timezones[0], timezones[1], timezones[2]);
