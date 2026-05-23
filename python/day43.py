68c436a20ecca43dcf1809a367ab3537934c0676
John Selwyn Borja <selwynborja11@gmail.com>
2026-05-23 15:38:31 +0800

    24 timezones list array of objects

---
+++ python/day43.py
@@ -0,0 +1,29 @@
+timezones = [
+  {"timezone": "UTC-12:00", "offset": -12.0, "name": "Etc/GMT-12"},
+  {"timezone": "UTC-11:00", "offset": -11.0, "name": "Pacific/Midway"},
+  {"timezone": "UTC-10:00", "offset": -10.0, "name": "Pacific/Honolulu"},
+  {"timezone": "UTC-09:00", "offset": -9.0, "name": "America/Anchorage"},
+  {"timezone": "UTC-08:00", "offset": -8.0, "name": "America/Los_Angeles"},
+  {"timezone": "UTC-07:00", "offset": -7.0, "name": "America/Denver"},
+  {"timezone": "UTC-06:00", "offset": -6.0, "name": "America/Chicago"},
+  {"timezone": "UTC-05:00", "offset": -5.0, "name": "America/New_York"},
+  {"timezone": "UTC-04:00", "offset": -4.0, "name": "America/Halifax"},
+  {"timezone": "UTC-03:00", "offset": -3.0, "name": "America/Sao_Paulo"},
+  {"timezone": "UTC-02:00", "offset": -2.0, "name": "Etc/GMT-2"},
+  {"timezone": "UTC-01:00", "offset": -1.0, "name": "Atlantic/Azores"},
+  {"timezone": "UTC", "offset": 0.0, "name": "Etc/UTC"},
+  {"timezone": "UTC+01:00", "offset": 1.0, "name": "Europe/Paris"},
+  {"timezone": "UTC+02:00", "offset": 2.0, "name": "Africa/Cairo"},
+  {"timezone": "UTC+03:00", "offset": 3.0, "name": "Asia/Moscow"},
+  {"timezone": "UTC+04:00", "offset": 4.0, "name": "Asia/Dubai"},
+  {"timezone": "UTC+05:00", "offset": 5.0, "name": "Asia/Karachi"},
+  {"timezone": "UTC+06:00", "offset": 6.0, "name": "Asia/Dhaka"},
+  {"timezone": "UTC+07:00", "offset": 7.0, "name": "Asia/Bangkok"},
+  {"timezone": "UTC+08:00", "offset": 8.0, "name": "Asia/Manila"},
+  {"timezone": "UTC+09:00", "offset": 9.0, "name": "Asia/Tokyo"},
+  {"timezone": "UTC+10:00", "offset": 10.0, "name": "Australia/Sydney"},
+  {"timezone": "UTC+11:00", "offset": 11.0, "name": "Pacific/Noumea"},
+  {"timezone": "UTC+12:00", "offset": 12.0, "name": "Pacific/Auckland"}
+]
+
+print(timezones[:3])
