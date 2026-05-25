3ace6bcec384b672a5292c9bb778d9bb8a729f04
John Selwyn Borja <selwynborja11@gmail.com>
2026-05-25 13:59:14 +0800

    fix space

--- js/day44.js
+++ js/day44.js
@@ -3,4 +3,3 @@ const text = "What the fuck is this? Is he dead?"
 // Replace words using an inline arrow function
 const cleanText = text.replace(/\b(fuck|dead)\b/gi, match => '*'.repeat(match.length));
 console.log(cleanText);
-
