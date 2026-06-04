1a960787fedd93eeb9d8cdf9954cc1b04ff44f4c
John Selwyn Borja <selwynborja11@gmail.com>
2026-06-04 18:40:46 +0800

    Check if string contains at least one uppercase

--- js/day52.js
+++ js/day52.js
@@ -1,5 +1,3 @@
-// Check if string contains 1 digit, 1 symbol, 1 uppercase letter and 1 lowercase and is more than 10 characters long
-
-const regex =  /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W).{11,}$/;
-const isValid = regex.test("Somewhereib1@");
+const regex = /(?=.*[A-Z])/;
+const isValid = regex.test("upperCase");
 console.log(isValid);
