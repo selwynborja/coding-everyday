91727a668400ce79bcbdffccf3ab7d2f0ce14301
John Selwyn Borja <selwynborja11@gmail.com>
2026-06-04 18:48:15 +0800

    Check if string contains uppercase letter

--- python/day52.py
+++ python/day52.py
@@ -1,7 +1,5 @@
 import re
 
-# Check if string contains 1 digit, 1 symbol, 1 uppercase letter and 1 lowercase and is more than 10 characters long
-
-pattern = r"^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W).{11,}$"
-is_valid = bool(re.match(pattern, "Somewhereib1@"))
+pattern = r"^(?=.*[A-Z])"
+is_valid = bool(re.match(pattern, "upperCase"))
 print(is_valid)
