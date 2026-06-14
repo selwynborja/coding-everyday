647cb36f31978083315be5bc29264b1e10654be6
John Selwyn Borja <selwynborja11@gmail.com>
2026-06-14 19:47:35 +0800

    upd

--- python/day64.py
+++ python/day64.py
@@ -6,26 +6,19 @@ def get_masked_input(prompt="Enter password: "):
     
     # Windows Implementation
     if sys.platform == "win32":
-        import msvcrt
-        while True:
-            ch = msvcrt.getch()
-            # Handle Enter key
-            if ch in (b'\r', b'\n'):
-                print()
-                break
-            # Handle Backspace
-            elif ch in (b'\x08', b'\x7f'):
-                if len(password) > 0:
-                    password = password[:-1]
-                    # Move back, overwrite with space, move back again
-                    sys.stdout.write('\b \b')
-                    sys.stdout.flush()
-            # Handle standard characters
-            elif ord(ch) >= 32:
-                password += ch.decode('utf-8')
-                sys.stdout.write('*')
-                sys.stdout.flush()
-            
+      import msvcrt
+      while True:
+          ch = msvcrt.getch()
+          # Handle Enter key
+          if ch in (b'\r', b'\n'):
+              print()
+              break
+          # Handle standard characters
+          elif ord(ch) >= 32:
+              password += ch.decode('utf-8')
+              sys.stdout.write('*')
+              sys.stdout.flush()
+          
     return password
 
 # Execute the function
